1cc6144b668b7808937fe8bf90e6bcc39922b80e
prof-leandro-teixeira <leandro_prof@yahoo.com>
2023-03-30 14:08:24 -0300

    Primeiros passos

---
+++ projeto/api-rest-springboot-java/src/main/java/com/leandro/SaudacaoController.java
@@ -0,0 +1,21 @@
+package com.leandro;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaudacaoController {
+	
+	private final static String template = "Olá, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/saudacao")
+	public Saudacao saudacao(@RequestParam(value = "name", defaultValue = "mundo!") String name) {
+		return new Saudacao(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+
+}
